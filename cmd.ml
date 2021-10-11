9bf853ba32ad0f540fb1ca48d645b41c45591cdf
afonso <aj.almeida@campusfct.unl.pt>
2021-10-11 11:53:54 +0100

    New module related to I/O only

---
+++ cmd.ml
@@ -0,0 +1,16 @@
+(* ------------------- COMMAND LINE ARGUMENTS -------------------- *)
+
+let usage_msg = "Usage: ./dlock [-v | -s | -ds] <file1> [<file2>] ... -o <output>"
+let verbose = ref false
+let simplified = ref false
+let procInput = ref false
+let ds = ref 0
+
+let speclist =
+    [("-v", Arg.Set verbose, "Output debug information");
+     ("-s", Arg.Set simplified, "Output a simpler representation of the process");
+     ("-p", Arg.Set procInput, "Input is a Proc type, which is translated to a Lamba type");
+     ("-ds", Arg.Int (fun i -> if (i!=1 && i!= 2) then raise (Arg.Bad ("Bad argument: Select deadlock resolution algorithm (1 or 2)")) else ds:=i), "Select deadlock resolution algorithm (1 or 2)");
+     ("  ", Arg.Unit (fun () -> ()), "Output the verdict only")]
+
+let cmdParse = Arg.parse speclist (fun x -> raise (Arg.Bad ("Bad argument: " ^ x))) usage_msg
